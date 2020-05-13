3c71e9899cc88157c7b43509066ebe0d1dbbc38e
nazimakber <nazim_virani@yahoo.com>
2020-05-14 01:31:57 +0500

    Updates

---
+++ precache-manifest.6efbabf6b9f8941242d62827e10689a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65826aeda1c0067ae8f6271c1f4216ae",
+    "url": "/nazimakber/react-ecomerce/index.html"
+  },
+  {
+    "revision": "686f7da382fee5ecfa8c",
+    "url": "/nazimakber/react-ecomerce/static/css/main.a1e02514.chunk.css"
+  },
+  {
+    "revision": "c4c01b13c3c23eac74a5",
+    "url": "/nazimakber/react-ecomerce/static/js/2.b06dabc8.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/nazimakber/react-ecomerce/static/js/2.b06dabc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686f7da382fee5ecfa8c",
+    "url": "/nazimakber/react-ecomerce/static/js/main.806ca780.chunk.js"
+  },
+  {
+    "revision": "4dcc41fd4d0fa2d5b86a",
+    "url": "/nazimakber/react-ecomerce/static/js/runtime-main.c42066ce.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/nazimakber/react-ecomerce/static/media/crown.ed28accb.svg"
+  }
+]);
